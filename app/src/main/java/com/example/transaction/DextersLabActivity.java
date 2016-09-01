daada93cf48a3ec3a48c02a7539a41b83f29d6f2
pranit <pranitsawant01@gmail.com>
2016-09-02 00:07:14 +0530

    1. added view for state loss 2. extracted strings from layout

--- app/src/main/java/com/example/transaction/DextersLabActivity.java
+++ app/src/main/java/com/example/transaction/DextersLabActivity.java
@@ -2,18 +2,27 @@ package com.example.transaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class DextersLabActivity extends BaseActivity {
+public class DextersLabActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dexters_lab);
-        findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(DexterActivity.getStartIntent(DextersLabActivity.this));
-            }
-        });
+        Button btnCommitVariants = (Button) findViewById(R.id.btnOtherCommitVariant);
+        Button btnStateLoss = (Button) findViewById(R.id.btnStateLoss);
+        btnCommitVariants.setOnClickListener(this);
+        btnStateLoss.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        if (i == R.id.btnOtherCommitVariant) {
+            startActivity(DexterActivity.getStartIntent(this));
+
+        } else if (i == R.id.btnStateLoss) {
+        }
     }
 }
