1ef1459009fb5426aa7425940ce3f8f3dd2b7dc0
pranit <pranitsawant01@gmail.com>
2016-09-08 10:04:13 +0530

    added click listeners and actions

--- app/src/main/java/com/example/transaction/DexterActivity.java
+++ app/src/main/java/com/example/transaction/DexterActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class DexterActivity extends BaseActivity implements RatFragment.OnFragmentInteractionListener{
+public class DexterActivity extends BaseActivity implements RatFragment.OnFragmentInteractionListener, View.OnClickListener {
 
     private static final String TAG = DexterActivity.class.getSimpleName();
 
-    public static Intent getStartIntent(Context context){
+    // commit vs commitAllowingStateLoss
+    private boolean allowStateLoss;
+    private boolean changeOnStop;
+
+    Button btnDisallowStateLoss, btnAllowStateLoss, btnNoStateLoss;
+
+    public static Intent getStartIntent(Context context) {
         return new Intent(context, DexterActivity.class);
     }
 
@@ -20,36 +28,56 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dexter);
-        fragmentTransaction(R.id.container,ADD_FRAGMENT,RatFragment.newInstance(null,null),false,null);
+        fragmentTransaction(R.id.container, ADD_FRAGMENT, RatFragment.newInstance(null, null), false, null);
 
-       final Handler handler =  new Handler(){
-           @Override
-           public void handleMessage(Message msg) {
-               super.handleMessage(msg);
-               if(msg.what == 100){
-                   getSupportFragmentManager().beginTransaction().replace(R.id.container,RatFragment.newInstance(null,null)).commitNow();
-                   Log.i(TAG, "handleMessage    : "+getSupportFragmentManager().getBackStackEntryCount());
-                   getSupportFragmentManager().popBackStackImmediate();
-                   Log.i(TAG, "handleMessage after pop-back-stack: Immediate"+getSupportFragmentManager().getBackStackEntryCount());
-                   getSupportFragmentManager().popBackStack();
-                   Log.i(TAG, "handleMessage pop-back: "+getSupportFragmentManager().getBackStackEntryCount());
-               }
-           }
-       };
+        // commit vs commitAllowingStateLoss
+        btnDisallowStateLoss = (Button) findViewById(R.id.disallow_state_loss);
+        btnAllowStateLoss = (Button) findViewById(R.id.allow_state_loss);
+        btnNoStateLoss = (Button) findViewById(R.id.no_state_loss);
+        btnDisallowStateLoss.setOnClickListener(this);
+        btnAllowStateLoss.setOnClickListener(this);
+        btnNoStateLoss.setOnClickListener(this);
+
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == 100) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, RatFragment.newInstance(null, null)).commitNow();
+                    Log.i(TAG, "handleMessage    : " + getSupportFragmentManager().getBackStackEntryCount());
+                    getSupportFragmentManager().popBackStackImmediate();
+                    Log.i(TAG, "handleMessage after pop-back-stack: Immediate" + getSupportFragmentManager().getBackStackEntryCount());
+                    getSupportFragmentManager().popBackStack();
+                    Log.i(TAG, "handleMessage pop-back: " + getSupportFragmentManager().getBackStackEntryCount());
+                }
+            }
+        };
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                for(int i = 0; i < 50; i++) {
+                for (int i = 0; i < 50; i++) {
                     fragmentTransaction(R.id.container, REPLACE_FRAGMENT, RatFragment.newInstance(null, null), true, String.valueOf(i));
                 }
-                Log.i(TAG, "run: "+getSupportFragmentManager().getBackStackEntryCount());
+                Log.i(TAG, "run: " + getSupportFragmentManager().getBackStackEntryCount());
                 handler.sendEmptyMessage(100);
             }
-        }, 3*1000);
+        }, 3 * 1000);
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.disallow_state_loss:
+                break;
+            case R.id.allow_state_loss:
+                break;
+            case R.id.no_state_loss:
+                break;
+        }
     }
 }
