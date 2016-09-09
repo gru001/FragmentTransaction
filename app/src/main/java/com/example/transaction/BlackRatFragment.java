e8b553e357d1b5426b41e82deec1f62c83bf1f47
pranit <pranitsawant01@gmail.com>
2016-09-10 00:23:57 +0530

    1. added necessary fragment 2. updated case for state loss

--- app/src/main/java/com/example/transaction/BlackRatFragment.java
+++ app/src/main/java/com/example/transaction/BlackRatFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RatFragment.OnFragmentInteractionListener} interface
+ * {@link BlackRatFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RatFragment#newInstance} factory method to
+ * Use the {@link BlackRatFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RatFragment extends Fragment {
+public class BlackRatFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class RatFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public RatFragment() {
+    public BlackRatFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class RatFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RatFragment.
+     * @return A new instance of fragment WhiteRatFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RatFragment newInstance(String param1, String param2) {
-        RatFragment fragment = new RatFragment();
+    public static BlackRatFragment newInstance(String param1, String param2) {
+        BlackRatFragment fragment = new BlackRatFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class RatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rat, container, false);
+        return inflater.inflate(R.layout.fragment_white_rat, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/transaction/DexterActivity.java
+++ app/src/main/java/com/example/transaction/DexterActivity.java
@@ -4,13 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class DexterActivity extends BaseActivity implements RatFragment.OnFragmentInteractionListener, View.OnClickListener {
+public class DexterActivity extends BaseActivity implements WhiteRatFragment.OnFragmentInteractionListener, View.OnClickListener {
 
     private static final String TAG = DexterActivity.class.getSimpleName();
 
@@ -18,7 +17,9 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
     private boolean allowStateLoss;
     private boolean changeOnStop;
 
-    Button btnDisallowStateLoss, btnAllowStateLoss, btnNoStateLoss;
+    Button btnDisallowStateLoss;
+    Button btnAllowStateLoss;
+    Button btnNoStateLoss;
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, DexterActivity.class);
@@ -28,7 +29,12 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dexter);
-        fragmentTransaction(R.id.container, ADD_FRAGMENT, RatFragment.newInstance(null, null), false, null);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, WhiteRatFragment.newInstance(null, null))
+                    .commit();
+        }
 
         // commit vs commitAllowingStateLoss
         btnDisallowStateLoss = (Button) findViewById(R.id.disallow_state_loss);
@@ -38,12 +44,12 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
         btnAllowStateLoss.setOnClickListener(this);
         btnNoStateLoss.setOnClickListener(this);
 
-        final Handler handler = new Handler() {
+        /*final Handler handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 if (msg.what == 100) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, RatFragment.newInstance(null, null)).commitNow();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, WhiteRatFragment.newInstance(null, null)).commitNow();
                     Log.i(TAG, "handleMessage    : " + getSupportFragmentManager().getBackStackEntryCount());
                     getSupportFragmentManager().popBackStackImmediate();
                     Log.i(TAG, "handleMessage after pop-back-stack: Immediate" + getSupportFragmentManager().getBackStackEntryCount());
@@ -51,22 +57,44 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
                     Log.i(TAG, "handleMessage pop-back: " + getSupportFragmentManager().getBackStackEntryCount());
                 }
             }
-        };
-        handler.postDelayed(new Runnable() {
+        };*/
+        /*handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < 50; i++) {
-                    fragmentTransaction(R.id.container, REPLACE_FRAGMENT, RatFragment.newInstance(null, null), true, String.valueOf(i));
+                    fragmentTransaction(R.id.container, REPLACE_FRAGMENT, WhiteRatFragment.newInstance(null, null), true, String.valueOf(i));
                 }
                 Log.i(TAG, "run: " + getSupportFragmentManager().getBackStackEntryCount());
                 handler.sendEmptyMessage(100);
             }
-        }, 3 * 1000);
+        }, 3 * 1000);*/
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (changeOnStop) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, BlackRatFragment.newInstance(null, null));
+
+            if (allowStateLoss) {
+                transaction.commitAllowingStateLoss();
+            } else {
+                transaction.commit();
+            }
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState: ");
     }
 
     @Override
@@ -83,6 +111,18 @@ public class DexterActivity extends BaseActivity implements RatFragment.OnFragme
                 startActivity(new Intent(DexterActivity.this, SecondActivity.class));
                 break;
             case R.id.no_state_loss:
+                changeOnStop = false;
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, BlackRatFragment.newInstance(null, null));
+
+                if (allowStateLoss) {
+                    transaction.commitAllowingStateLoss();
+                } else {
+                    transaction.commit();
+                }
+
+                startActivity(new Intent(DexterActivity.this, SecondActivity.class));
                 break;
         }
     }
